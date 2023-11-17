f80749cff41f6e13e48fc519e881d65936e2e81b
Lucas Castro <castroclucas@gmail.com>
2023-11-17 15:12:07 -0300

    feat: first commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log(Bun.env)
+import questions from "./questions"
+
+const port = parseInt(process.env.PORT || "8080")
+
+const getShuffledArray = () => {
+	const array = [...questions]
+
+	for (let i = array.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1))
+		;[array[i], array[j]] = [array[j], array[i]]
+	}
+
+	return array
+}
+
+Bun.serve({
+	port,
+	fetch(req) {
+		const shuffledArray = getShuffledArray().slice(0, 5)
+		return new Response(JSON.stringify(shuffledArray), {
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+	},
+})

07faf270c3c8ffe17b76476c8c3b717ca707a987
Lucas Castro <castroclucas@gmail.com>
2024-02-04 21:39:59 -0300

    refactor: adjust comments

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-import questions from "./questions"
+import questions from "./questions";
 
-const port = parseInt(process.env.PORT || "8080")
+const port = parseInt(process.env.PORT || "8080");
 
 const getShuffledArray = () => {
-	const array = [...questions]
+	const array = [...questions];
 
 	for (let i = array.length - 1; i > 0; i--) {
-		const j = Math.floor(Math.random() * (i + 1))
-		;[array[i], array[j]] = [array[j], array[i]]
+		const j = Math.floor(Math.random() * (i + 1));
+		[array[i], array[j]] = [array[j], array[i]];
 	}
 
-	return array
+	return array;
 }
 
 Bun.serve({
 	port,
-	fetch(req) {
+	fetch(_) {
 		const shuffledArray = getShuffledArray().slice(0, 5)
 		return new Response(JSON.stringify(shuffledArray), {
 			headers: {
@@ -23,6 +23,6 @@ Bun.serve({
 				"Access-Control-Allow-Origin": "*",
 				"Access-Control-Allow-Methods": "GET",
 			},
-		})
+		});
 	},
-})
+});

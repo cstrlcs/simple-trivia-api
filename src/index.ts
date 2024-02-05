eecc2bf5c1683761174d4abf5f15c27fcfc45e06
Lucas Castro <castroclucas@gmail.com>
2024-02-04 23:14:33 -0300

    refactor: apply stricter ts & biome rules

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,24 @@
-import questions from "./questions";
+import { QUESTIONS, Question } from "./questions";
+import Bun from "bun";
 
 const port = parseInt(process.env.PORT || "8080");
 
 const getShuffledArray = () => {
-	const array = [...questions];
+	const array = [...QUESTIONS];
 
 	for (let i = array.length - 1; i > 0; i--) {
 		const j = Math.floor(Math.random() * (i + 1));
-		[array[i], array[j]] = [array[j], array[i]];
+
+		[array[i], array[j]] = [array[j] as Question, array[i] as Question];
 	}
 
 	return array;
-}
+};
 
 Bun.serve({
 	port,
 	fetch(_) {
-		const shuffledArray = getShuffledArray().slice(0, 5)
+		const shuffledArray = getShuffledArray().slice(0, 5);
 		return new Response(JSON.stringify(shuffledArray), {
 			headers: {
 				"Content-Type": "application/json",
